8ef9d4af0ba9aa2eb7474f96fe97fb3861b5d11c
Muhaimenul Islam <90051115+muhaimenuledu@users.noreply.github.com>
2025-02-14 17:23:52 +0600

    Update CustomerSync.php

--- Controller/Adminhtml/SageConnector/CustomerSync.php
+++ Controller/Adminhtml/SageConnector/CustomerSync.php
@@ -6,70 +6,87 @@ use Magento\Backend\App\Action;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\View\Result\PageFactory;
-use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
 
 class CustomerSync extends Action
 {
     protected $messageManager;
     protected $resultPageFactory;
     protected $_scopeConfig;
-    protected $_customerFactory;
 
     public function __construct(
         Action\Context $context,
         ManagerInterface $messageManager,
         PageFactory $resultPageFactory,
-        ScopeConfigInterface $scopeConfig,
-        CollectionFactory $customerFactory
+        ScopeConfigInterface $scopeConfig
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
         $this->messageManager = $messageManager;
         $this->_scopeConfig = $scopeConfig;
-        $this->_customerFactory = $customerFactory;
     }
 
     public function execute()
     {
-        $customerCollection = $this->_customerFactory->create();
+        // Check if the module is enabled
+        // $isEnabled = $this->_scopeConfig->getValue('hww_SageConnector/general/enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        // if (!$isEnabled) {
+        //     $this->messageManager->addErrorMessage(__('Sage Connector module is disabled in the configuration.'));
+        //     $this->_redirect($this->_redirect->getRefererUrl());
+        //     return;
+        // }
 
-        foreach ($customerCollection as $customer) {
-            $fname = $customer->getFirstname();
-            $lname = $customer->getLastname();
-            $name = $fname . ' ' . $lname;
-            $email = $customer->getEmail();
-            $this->sendToSage($name, $email);
-        }
-        $this->messageManager->addSuccessMessage(__('Customer Data synced with Sage successfully.'));
-        $this->_redirect($this->_redirect->getRefererUrl());
-    }
-
-    protected function sendToSage($name, $email)
-    {
-        $isEnabled = $this->_scopeConfig->getValue('hww_SageConnector/general/enable', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        // Get the Sage API URL from configuration
         $url = $this->_scopeConfig->getValue('hww_SageConnector/general/url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-
-        if (!$isEnabled) {
+        if (empty($url)) {
+            $this->messageManager->addErrorMessage(__('Sage API URL is not configured.'));
+            $this->_redirect($this->_redirect->getRefererUrl());
             return;
         }
 
+        // Fixed data for syncing one customer
+        $postData = json_encode([
+            "ARDivisionNo" => "10",
+            "CustomerNo" => "C123465"
+        ]);
+
+        // Initialize cURL session
         $curl = curl_init();
+
+        // Set cURL options
         curl_setopt_array($curl, [
-            CURLOPT_URL => $url . '/api/customerSync',
+            CURLOPT_URL => $url . '/customer/create/',
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => json_encode([
-                "params" => [
-                    "name" => $name,
-                    "email" => $email
-                ]
-            ]),
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postData,
             CURLOPT_HTTPHEADER => [
                 'Content-Type: application/json'
             ],
+            // Disable SSL verification for self-signed certificates
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
         ]);
 
+        // Execute cURL request and get response
         $response = curl_exec($curl);
+        // dd($response);
+
+        // Check for cURL errors
+        if (curl_errno($curl)) {
+            $errorMessage = 'cURL Error: ' . curl_error($curl);
+            $this->messageManager->addErrorMessage($errorMessage);
+        } else {
+            $this->messageManager->addSuccessMessage(__($response));
+        }
+
+        // Close cURL session
         curl_close($curl);
+
+        // Redirect back to the referring page
+        $this->_redirect($this->_redirect->getRefererUrl());
     }
 }
