7023ffcd7b7d98bd0569f550f062525b0161f25a
Muhaimenul Islam <90051115+muhaimenuledu@users.noreply.github.com>
2025-05-08 17:54:55 +0600

    Add files via upload

---
+++ Controller/Adminhtml/SageConnector/GetProduct.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Harriswebworks\SageConnector\Controller\Adminhtml\Sageconnector;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+
+class GetProduct extends Action
+{
+    protected $jsonFactory;
+
+    public function __construct(
+        Context $context,
+        JsonFactory $jsonFactory
+    ) {
+        parent::__construct($context);
+        $this->jsonFactory = $jsonFactory;
+    }
+
+    public function execute()
+    {
+        $result = $this->jsonFactory->create();
+
+        try {
+            $apiUrl = 'https://52.186.11.198:88/TestCase/Product/getProduct.php';
+
+            // Initialize cURL
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => $apiUrl,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_SSL_VERIFYPEER => false, // Disable SSL certificate validation
+                CURLOPT_SSL_VERIFYHOST => false, // Disable host validation
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ]);
+
+            $response = curl_exec($curl);
+
+            $this->messageManager->addNoticeMessage(__($response));
+
+            if (curl_errno($curl)) {
+                throw new \Exception('Curl error: ' . curl_error($curl));
+            }
+
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            curl_close($curl);
+        } 
+        catch (\Exception $e) {
+            return $this->jsonFactory->create()->setData([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return $this->_redirect($this->_redirect->getRefererUrl());
+    }
+}
